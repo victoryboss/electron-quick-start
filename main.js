70935a5fc47e8bf97aa8f287729775d3cb69b963
xiaoxi <xihengfei@126.com>
2019-12-18 16:15:42 +0800

    Update main.js
    添加自定义菜单，以及租金城渲染进程通信

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu ,ipcMain} = require('electron')
 const path = require('path')
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+// 在package.json中开启主进程调试
+// "start": "electron --inspect=5858 .",
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        alwaysOnTop: true,
+        width: 800,
+        height: 600,
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+        webPreferences: {
+            devTools:true,  // 开启调试 ，
+            nodeIntegration: true  // 渲染进程开启requir关键字s有效 ，
+        }
+    })
+    mainWindow.setAlwaysOnTop(true, 'modal-panel', 9999)
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+
+    mainWindow.webContents.openDevTools({ mode: 'bottom' })
 }
 
 // This method will be called when Electron has finished
@@ -38,16 +45,74 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+var template = [{
+    label: 'Edit',
+    submenu: [
+        {
+            label: 'item1',
+            click: function () {
+                // mainWindow.webContents.on('did-finish-load', function() {
+                //     mainWindow.webContents.send('ping', 'whoooooooh!');
+                // });
+                //向渲染进程发送信息
+                mainWindow.webContents.send('ping', 'whoooooooh!');
+            }
+        },
+        {
+            type: 'separator'
+        }
+    ]
+}];
+
+var menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+
+// main.js
+const createDockMenu = () => {
+    const dockTempalte = [
+        {
+            label: 'New Window',
+            click () {
+                console.log('New Window');
+            }
+        }, {
+            label: 'New Window with Settings',
+            submenu: [
+                { label: 'Basic' },
+                { label: 'Pro' }
+            ]
+        },
+        {
+            label: 'New Command...'
+        }
+    ];
+
+    const dockMenu = Menu.buildFromTemplate(dockTempalte);
+    app.dock.setMenu(dockMenu);
+}
+
+app.on('ready', function() {
+    createDockMenu();
+});
+
+//接收渲染进程发来的信息
+ipcMain.on('asynchronous-message', (event, arg) => {
+    console.log(arg) // prints "ping"
+    event.reply('asynchronous-reply', 'async pong')
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+    console.log(arg) // prints "ping"
+    event.returnValue = 'sync pong'
+})
